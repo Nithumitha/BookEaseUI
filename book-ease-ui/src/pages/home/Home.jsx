125f3c89961483d5e63e35db72cc6ffef2138073
R, Nithumitha <82254716+Nithumitha@users.noreply.github.com>
2025-06-29 12:40:05 +0530

    Email section and Footer section

--- book-ease-ui/src/pages/home/Home.jsx
+++ book-ease-ui/src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import Featured from "../../components/featured/Featured"
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
+import Footer from "../../components/footer/Footer"
 import Header from "../../components/header/Header"
+import MailList from "../../components/mailList/MailList"
 import Navbar from "../../components/navbar/Navbar"
 import PropertyList from "../../components/propertyList/PropertyList"
 import "./home.css"
@@ -11,11 +13,14 @@ function Home() {
             <Navbar />
             <Header />
             <div className="homeContainer">
+                <h1 className="homeTitle">Popular destinations with travellers from India</h1>
                 <Featured />
                 <h1 className="homeTitle">Browse by property type</h1>
                 <PropertyList />
                 <h1 className="homeTitle">Homes guests love</h1>
                 <FeaturedProperties />
+                <MailList />
+                <Footer />
             </div>
         </div>
     )
