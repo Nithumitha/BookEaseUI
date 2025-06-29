93a9d09d371b56f739397b2f3ef9d8123d70eb8b
R, Nithumitha <82254716+Nithumitha@users.noreply.github.com>
2025-06-29 15:41:11 +0530

    Search component

--- book-ease-ui/src/pages/list/List.jsx
+++ book-ease-ui/src/pages/list/List.jsx
@@ -1,11 +1,55 @@
+import "./list.css"
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
+import { useLocation } from 'react-router-dom'
+import { useState } from "react"
+import format from "date-fns/format"
+import { DateRange } from "react-date-range"
 
-const List = () =>{
+const List = () => {
+
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state?.destination || "");
+  const [date, setDate] = useState(location.state?.date || [
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]
+  );
+  const [options, setOptions] = useState(location.state?.options || {});
+  const [openDate, setOpenDate] = useState(false);
+
   return (
     <div>
       <Navbar />
-      <Header type="list"/>
+      <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input type="text" placeholder={destination} />
+            </div>
+            <div className="lsItem">
+              <label>Check-in Check-out</label>
+              <span onClick={() => setOpenDate(!openDate)}>
+                {`${format(date[0].startDate, "dd-MM-yyyy")} to ${format(date[0].endDate, "dd-MM-yyyy")}`}
+              </span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
+            </div>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   )
 }
