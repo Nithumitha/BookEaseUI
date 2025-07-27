7df13162189b58a901f7aef481d9f720db4a3db9
R, Nithumitha <nithuramesh26@gmail.com>
2025-07-27 13:02:50 +0530

    Added the required API's

--- book-ease-ui/src/components/navbar/Navbar.jsx
+++ book-ease-ui/src/components/navbar/Navbar.jsx
@@ -1,15 +1,18 @@
 import "./navbar.css"
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
-        <div className="navContainer">
-            <span className="logo">BookEase.com</span>
-            <div className="navItems">
-            <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
-            </div>
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">BookEase.com</span>
+        </Link>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
+      </div>
     </div>
   )
 }
